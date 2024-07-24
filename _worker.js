513ecdb3f13411f8a5366344d7cc9c0c8d33551d
atecf <atetestcf@gmail.com>
2024-07-24 08:56:58 +0800

    Update _worker.js
    Update UUID

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ca562ab3-96cd-4b58-b216-c53eb78801e8';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
